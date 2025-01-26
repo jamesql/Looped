213431bd3e29cae0018fb0ea41955e07e1dd5c96
James Ash <thejamesash1@gmail.com>
2025-01-25 22:56:30 -0500

    add join a server functionality

--- API/src/routers/API.ts
+++ API/src/routers/API.ts
@@ -11,29 +11,34 @@ import { OPCodes } from "../Util/OPCodes";
 const router: Router = express.Router();
 const _prisma = Prisma.getInstance();
 
-export const validateToken = async (token: string, res: Response): Promise<Auth.TokenValidation> => {
-    let { userId, exp } = await validateAccessToken(token);
+export const validateToken = async (
+  token: string,
+  res: Response
+): Promise<Auth.TokenValidation> => {
+  let { userId, exp } = await validateAccessToken(token);
 
-    if (userId === undefined) {
-        res.status(401).send("Invalid credentials.");
-        return {valid: false, userId: null};
-    }
+  if (userId === undefined) {
+    res.status(401).send("Invalid credentials.");
+    return { valid: false, userId: null };
+  }
 
-    let isExpired = Date.now() / 1000 > exp;
+  let isExpired = Date.now() / 1000 > exp;
 
-    if (isExpired) {
-        res.status(401).send("Unauthorized.")
-        return {valid: false, userId: null};
-    }
+  if (isExpired) {
+    res.status(401).send("Unauthorized.");
+    return { valid: false, userId: null };
+  }
 
-    return {valid: true, userId: userId};
+  return { valid: true, userId: userId };
 };
 
-router.post("/create-server", [
+router.post(
+  "/create-server",
+  [
     header("Authorization").notEmpty().withMessage("No Token Provided."),
-    body("name").notEmpty().withMessage("No name provided.")
-],
- async (req: Request, res: Response) => {
+    body("name").notEmpty().withMessage("No name provided."),
+  ],
+  async (req: Request, res: Response) => {
     // validate header
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -47,50 +52,138 @@ router.post("/create-server", [
     if (!validation.valid || validation.userId === null) return;
 
     let user = await _prisma.user.findUnique({
-        where: {
-            id: validation.userId
-        }        
+      where: {
+        id: validation.userId,
+      },
     });
 
     if (user === null) {
-        res.status(401).send("User not found.");
-        return;
+      res.status(401).send("User not found.");
+      return;
     }
 
     // create server in database
     let server = await _prisma.server.create({
-        data: {
-            id: v4(),
-            name: name,
-            ownerId: user.id
-        }
+      data: {
+        id: v4(),
+        name: name,
+        ownerId: user.id,
+      },
     });
 
     // add user to server
     let member = await _prisma.serverMember.create({
-        data: {
-            id: v4(),
-            userId: user.id,
-            serverId: server.id
-        }
+      data: {
+        id: v4(),
+        userId: user.id,
+        serverId: server.id,
+      },
     });
 
     let payload = {
-        op: OPCodes.SERVER_CREATE,
-        d: {
-            id: server.id,
-            name: server.name,
-            ownerId: user.id
-        }
-    }
+      op: OPCodes.SERVER_CREATE,
+      d: {
+        id: server.id,
+        name: server.name,
+        ownerId: user.id,
+      },
+    };
 
     await publishToChannel(`user-events:${user.id}`, payload);
     await updateUserState(user.id);
     res.status(200).send(payload.d);
     return;
- });
+  }
+);
 
-router.post("/join-server", (req: Request, res: Response) => {});
+router.post(
+  "/join-server",
+  [
+    header("Authorization").notEmpty().withMessage("No Token Provided."),
+    body("serverId").notEmpty().withMessage("No server id provided."),
+  ],
+  async (req: Request, res: Response) => {
+    // validate header
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    let token = req.header("Authorization") || "INVALID TOKEN";
+    let { serverId } = req.body;
+
+    let validation: Auth.TokenValidation = await validateToken(token, res);
+    if (!validation.valid || validation.userId === null) return;
+
+    let user = await _prisma.user.findUnique({
+      where: {
+        id: validation.userId,
+      },
+    });
+
+    if (user === null) {
+      res.status(401).send("User not found.");
+      return;
+    }
+
+    let server = await _prisma.server.findUnique({
+      where: {
+        id: serverId,
+      },
+    });
+
+    if (server === null) {
+      res.status(401).send("Server not found.");
+      return;
+    }
+
+    // make sure user is not banned/already in server
+    // todo:
+
+    let mQuery = await _prisma.serverMember.findFirst({
+      where: {
+        userId: user.id,
+        serverId: server.id,
+      },
+    });
+
+    if (mQuery !== null) {
+      res.status(401).send("Already a member of this server.");
+      return;
+    }
+
+    // add user to server
+    let member = await _prisma.serverMember.create({
+      data: {
+        id: v4(),
+        userId: user.id,
+        serverId: server.id,
+      },
+    });
+
+    let scPayload = {
+      op: OPCodes.SERVER_CREATE,
+      d: {
+        id: server.id,
+        name: server.name,
+        ownerId: user.id,
+      },
+    };
+
+    let mjPayload = {
+      op: OPCodes.SERVER_MEMBER_ADD,
+      d: {
+        id: server.id,
+        user: user,
+      },
+    };
+
+    await publishToChannel(`user-events:${user.id}`, scPayload);
+    await publishToChannel(`server-events:${server.id}`, mjPayload);
+    await updateUserState(user.id);
+    res.status(200).send(scPayload.d);
+  }
+);
 
 router.post("/leave-server", (req: Request, res: Response) => {});
 
