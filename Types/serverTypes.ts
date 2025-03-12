e123d0bce121469ac307bf33750cddd298fd2ae7
James Ash <thejamesash1@gmail.com>
2025-03-12 10:42:56 -0400

    [types] set some optional server data on server types

--- Types/serverTypes.ts
+++ Types/serverTypes.ts
@@ -3,8 +3,8 @@ import { User } from "./userTypes";
 export interface Server {
     id: string;
     name: string;
-    createdAt: string;
-    updatedAt: string;
+    createdAt: Date;
+    updatedAt: Date;
     ownerId: string;
     owner: User;
     members: User[];
@@ -13,31 +13,29 @@ export interface Server {
     icon?: string;
     banner?: string;
     description?: string;
-    public: boolean;
-    onlineMembers: User[];
+    onlineMembers?: User[];
     roles: Role[];
 }
 
 export interface Channel {
     id: string;
     name: string;
-    createdAt: string;
-    updatedAt: string;
+    createdAt: Date;
+    updatedAt: Date;
     serverId: string;
-    server: Server;
-    members: User[];
+    server?: Server;
     messages: Message[];
 }
 
 export interface Message {
     id: string;
     content: string;
-    createdAt: string;
-    updatedAt: string;
+    createdAt: Date;
+    updatedAt: Date;
     authorId: string;
     author: User;
     channelId: string;
-    channel: Channel;
+    channel?: Channel;
 }
 
 export interface Role {
@@ -45,5 +43,5 @@ export interface Role {
     name: string;
     permissions: string[];
     serverId: string;
-    server: Server;
+    server?: Server;
 }
