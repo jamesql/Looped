6768da049dffb2fcfd2870f02ac8b06978522d39
James Ash <thejamesash1@gmail.com>
2025-03-11 21:01:40 -0400

    [socket] start adding websocket base

---
+++ Socket/@types/global.d.ts
@@ -0,0 +1,34 @@
+import { Server } from "http";
+import LoopedSession from "../../Types/sessionTypes";
+import Redis from "ioredis";
+
+export {};
+
+// Global tyoes
+declare global {
+
+  namespace Socket {
+    type Server = import("ws").Server;
+    type WebSocket = import("ws");
+
+    interface SocketServer extends Server {
+      clients: Set<SocketClient>;
+    }
+
+    interface SocketClient extends WebSocket {
+      type: "client";
+      authenticated: boolean;
+      session: LoopedSession;
+      address: String;
+      activeSubscriptions: Set<string>;
+      subscriber: Redis;
+
+      props: {
+        sequence: number;
+        lastHeartbeat: number;
+      };
+
+      sendAsync(data: any): Promise<void>;
+    }
+  }
+}
