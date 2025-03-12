8be085d4b6cfc1253eeb17d18e590935d5142198
James Ash <thejamesash1@gmail.com>
2025-03-11 23:26:09 -0400

    [api] add base files

---
+++ API/index.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors"
+
+// include .env
+require('dotenv').config();
+
+const app: Express = express();
+
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.use("/api", require("./routers/API"));
+app.use("/auth", require("./routers/Auth"));
+
+// port should change later, from env file
+app.listen(80, () => {
+  console.log(`[$api] API Server Started.`);
+});
