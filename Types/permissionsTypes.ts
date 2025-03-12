be500323a8cc8bbc8bb25c399da0992c0a3de229
James Ash <thejamesash1@gmail.com>
2025-03-12 12:05:18 -0400

    [util] add permission types

--- Types/permissionsTypes.ts
+++ Types/permissionsTypes.ts
@@ -1,1 +1,5 @@
 export type Permissions = "OWNER" | "ADMIN" | "MANAGER" | "MEMBER";
+export const Owner: Permissions = "OWNER";
+export const Admin: Permissions = "ADMIN";
+export const Manager: Permissions = "MANAGER";
+export const Member: Permissions = "MEMBER";
