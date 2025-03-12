734fefd1476bbcdcfc5758a6813281b6ce3acb9f
James Ash <thejamesash1@gmail.com>
2025-03-12 09:31:40 -0400

    [api] add routes

--- API/routers/API.ts
+++ API/routers/API.ts
@@ -4,4 +4,14 @@ const { body, validationResult, header } = require("express-validator");
 
 const router: Router = express.Router();
 
+router.use("/user", require("./apiRoutes/UserRoutes"));
+router.use("/channel", require("./apiRoutes/ChannelRoutes"));
+router.use("/message", require("./apiRoutes/MessageRoutes"));
+router.use("/role", require("./apiRoutes/RoleRoutes"));
+router.use("/server", require("./apiRoutes/ServerRoutes"));
+router.use("/portfolio", require("./apiRoutes/PortfolioRoutes"));
+router.use("/job", require("./apiRoutes/JobRoutes"));
+
+router.use("/test", require("./apiRoutes/TestRoutes"));
+
 module.exports = router;
---
+++ API/routers/apiRoutes/RoleRoutes.ts
@@ -0,0 +1,7 @@
+import express, {Router,  Express, Request, Response } from "express";
+const { body, validationResult, header } = require("express-validator");
+
+
+const router: Router = express.Router();
+
+module.exports = router;
