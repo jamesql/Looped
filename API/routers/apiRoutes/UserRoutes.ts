7f15e031cb4e34175da10debaa1ef31936ab2c73
James Ash <thejamesash1@gmail.com>
2025-03-12 18:02:34 -0400

    [api/users] add get-user-data and set status routes

--- API/routers/apiRoutes/UserRoutes.ts
+++ API/routers/apiRoutes/UserRoutes.ts
@@ -1,9 +1,69 @@
 import express, {Router,  Express, Request, Response } from "express";
 const { body, validationResult, header } = require("express-validator");
-
+import { validateToken } from "../../data/token";
+import UserService from "../../data/users";
+import { redisInstance } from "../../data/redis";
 
 const router: Router = express.Router();
 
+// Get user data
+router.get("/get-user-data", [
+    header("Authorization").isString().isLength({ min: 1 }),
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+    }
 
+    const token = req.header("Authorization");
+    const result = await validateToken(token);
+
+    if (!result || !result.valid) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+    }
+
+    const user = await UserService.getAllUserData(result.userId);
+
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
+
+    res.status(200).json(user);
+    return;
+    
+});
+
+// set user status
+router.post("/status", [
+    header("Authorization").isString().isLength({ min: 1 }),
+    body("status").isString().isLength({ min: 1 }),
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+    }
+
+    const token = req.header("Authorization");
+    const result = await validateToken(token);
+
+    if (!result || !result.valid) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+    }
+
+    const user = await UserService.getUserById(result.userId);
+
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
+
+    // set status in redis
+    redisInstance.set(`user:${user.id}:status`, req.body.status);
+});
 
 module.exports = router;
