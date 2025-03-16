7193e9f385ad74bee28e6e5ca83af2da1021d4f7
James Ash <thejamesash1@gmail.com>
2025-03-16 16:30:10 -0400

    [react] going to refactor to next.js + react.js, pushing for completition

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
 
        <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={< Login/>} />
+          <Route path="/signup" element={< Signup/>} />
           {/* Add more routes here as needed */}
        </Routes>
       </div>
--- React/src/pages/Home.tsx
+++ React/src/pages/Home.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import "../page-styles/home.css";
 import "../page-styles/default.css";
 
+import { useNavigate } from 'react-router';
 
 const Home: React.FC = () => {
+    const navigate = useNavigate();
     return (
         <div className="home-container">
             <img className="logo" src="logo_main.jpg" alt="Logo" />
             <div className="buttons">
-                <button className="button" onClick={() => ""}>Login</button>
-                <button className="button" onClick={() => ""}>Sign Up</button>
+                
+                <button className="button" onClick={() => navigate("/login")}>Login</button>
+                <button className="button" onClick={() => navigate("/signup")}>Sign Up</button>
             </div>
         </div>
     );
