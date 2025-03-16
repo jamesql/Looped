26533f3e771dd084f665dc7bce46169b000495ad
James Ash <thejamesash1@gmail.com>
2025-03-16 16:19:45 -0400

    [react] add home page style

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+
+       <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes here as needed */}
+       </Routes>
+      </div>
+    </Router>
   );
 }
 
--- React/src/pages/Home.tsx
+++ React/src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import "../page-styles/default.css";
 import "../page-styles/home.css";
 
 const Home: React.FC = () => {
     return (
         <div className="home-container">
-            <img src="../../../Assets/logo_main.jpg" alt="Logo" />
+            <img className="logo" src="logo_main.jpg" alt="Logo" />
             <div className="buttons">
                 <button onClick={() => ""}>Login</button>
                 <button onClick={() => ""}>Sign Up</button>
