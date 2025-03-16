d134c50674d3458b2bf9f1a88fe905f700ec6571
James Ash <thejamesash1@gmail.com>
2025-03-16 15:54:16 -0400

    [react] format loader.tsx

--- React/src/components/loader.tsx
+++ React/src/components/loader.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import '../styles/loader.css';
+import React from "react";
+import "../styles/loader.css";
 
 const Loader: React.FC = () => {
-    return (
-        <div className="loader">
-        <div className="circle"></div>
-        <div className="circle"></div>
+  return (
+    <div className="loader">
+      <div className="circle"></div>
+      <div className="circle"></div>
     </div>
-    );
+  );
 };
 
 export default Loader;
