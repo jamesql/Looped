07e32d9c412377c7742c9497c2f846b1d31051b9
James Ash <thejamesash1@gmail.com>
2025-03-16 16:02:33 -0400

    [react] add home page

--- React/src/pages/Home.tsx
+++ React/src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import "../page-styles/default.css";
+import "../page-styles/home.css";
 
 const Home: React.FC = () => {
     return (
-        <div>
-            
+        <div className="home-container">
+            <img src="../../../Assets/logo_main.jpg" alt="Logo" />
+            <div className="buttons">
+                <button onClick={() => ""}>Login</button>
+                <button onClick={() => ""}>Sign Up</button>
+            </div>
         </div>
     );
 };
