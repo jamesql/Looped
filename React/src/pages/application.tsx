f1e6507ab7c2a4ba7c0ec3576343f3b5cd5036a2
James Ash <thejamesash1@gmail.com>
2025-03-18 09:56:39 -0400

    [react] add message's and fix chat input margin

--- React/src/pages/application.tsx
+++ React/src/pages/application.tsx
@@ -101,7 +101,21 @@ const Application: React.FC = () => {
               <div className={classes.messages}>
 
                 <div className={classes.message}>
+                  <img className={classes.squircle} src="https://as1.ftcdn.net/v2/jpg/05/56/29/36/1000_F_556293653_e9P80XtK4yyDd8WU1vRtdqSU1Vym7zoX.jpg" alt="" />
+                  <div className={classes.message_details}>
+                    <div className={classes.author_details}>
+                        <h3>James Ash</h3>
+                        <p>03/14/2025 - 11:15 AM</p>
+                    </div>
 
+                    <div className={classes.message_content}>
+                        <p>
+                        Hello, this is a test message to see how the chat looks like. Hello, this is a test message to see how the chat looks like. Hello, this is a test message to see how the chat looks like. Hello, this is a test message to see how the chat looks like.
+                        </p>
+                    </div>
+
+                  </div>  
+                </div>
                 </div>
 
                 <div className={classes.chat_input}>
@@ -114,7 +128,7 @@ const Application: React.FC = () => {
                     </button>
                 </div>
 
-              </div>
+              
 
 
 
