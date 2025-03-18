191dda0d387c0fa287434ca331226655dc27e79b
James Ash <thejamesash1@gmail.com>
2025-03-17 23:54:14 -0400

    [react] add check for access and refresh token

--- React/src/pages/application.tsx
+++ React/src/pages/application.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import WebSocketComponent from '@/components/WebSocket';
 import { OpCodeHandler } from '@/util/ws';
 import { OPCodes } from '../../../Types/socketTypes';
+import Cookies from 'js-cookie';
 
 const Application: React.FC = () => {
 
     // create the map of listeners
     const listeners = new Map<number, OpCodeHandler[]>();
+
+    // check if access and refresh token are in cookie
+    useEffect(() => {
+        const accessToken = Cookies.get('access_token');
+        const refreshToken = Cookies.get('refresh_token');
+
+        if (!accessToken || !refreshToken) {
+            // redirect to login page
+            window.location.href = "/login";
+        }
+    }, []);
 
     // Example handler
     const exampleHandler: OpCodeHandler = (data) => {
