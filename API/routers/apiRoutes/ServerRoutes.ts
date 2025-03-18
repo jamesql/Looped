e6ea7f98049440e80702ed04526030b739ddeb28
James Ash <thejamesash1@gmail.com>
2025-03-17 21:07:45 -0400

    [routes] start adding redis to servers and add join route

--- API/routers/apiRoutes/ServerRoutes.ts
+++ API/routers/apiRoutes/ServerRoutes.ts
@@ -4,6 +4,9 @@ import TokenUtil from "../../../Util/Token";
 import ServerService from "../../data/servers";
 import UserService from "../../data/users";
 import { validateToken } from "../../data/token";
+import { redisInstance } from "../../data/redis";
+import { OPCodes } from "../../../Types/socketTypes";
+
 
 const tokenUtil = new TokenUtil();
 
@@ -65,6 +68,14 @@ router.post("/create", [
 
     // join user to server
     await ServerService.addMember(newServer.id, user.id);
+
+    // publish to redis {"op":OPCodes, "d":{"type":"serverJoin", "server":newServer}}
+    redisInstance.publish(`user:${user.id}:events`, JSON.stringify({
+        op: OPCodes.SERVER_CREATE,
+        d: {
+            server: newServer
+        }
+    }));
 
     // return server
     res.status(200).json(newServer);
@@ -192,5 +203,56 @@ router.post("/delete", [
     return;
 
 });
+
+// join server route
+router.post("/join", [
+    header("Authorization").isString().isLength({min: 1}),
+    body("code").isString().isLength({min: 1}),
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+    }
+    // validate access token
+    const token = req.header("Authorization");
+    const result = await validateToken(token);
+    if (!result || !result.valid) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+    }
+
+    // get user
+    const user = await UserService.getUserById(result.userId);
+    // make sure user exists
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
+
+    // get server by invite code
+    const server = await ServerService.getServerByInviteCode(req.body.code);
+    // make sure server exists
+    if (!server) {
+        res.status(404).json({ error: "Server not found" });
+        return;
+    }
+    // add user to server
+    await ServerService.addMember(server.id, user.id);
+
+    // publish to redis {"op":OPCodes, "d":{"type":"serverJoin", "server":newServer}}
+        redisInstance.publish(`user:${user.id}:events`, JSON.stringify({
+            op: OPCodes.SERVER_CREATE,
+            d: {
+                server: server
+            }
+        }));
+    
+    // return server
+    res.status(200).json(server);
+    return;
+});
+
+
 
 module.exports = router;
