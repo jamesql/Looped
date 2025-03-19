62ff05c5fb868b58addd83ecaf312b7935dc7e27
James Ash <thejamesash1@gmail.com>
2025-03-18 22:21:20 -0400

    [socket] use session update queue

--- Socket/modules/message.ts
+++ Socket/modules/message.ts
@@ -72,6 +72,8 @@ export default async (
         let _subscriber = new RedisPubSub();
         client.subscriber = _subscriber;
         
+        // send to SessionUpdateQueue
+        client.subscriber.publish(`SessionUpdateQueue`, decode["userId"]);
   
         // get session and store in client.session
         let _session = JSON.parse( await client.subscriber.get(`user:${decode["userId"]}:session`) );
