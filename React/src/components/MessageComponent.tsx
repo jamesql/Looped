3c5c36cbaa758e4f5209bd4de6cf1d7b281a53fd
James Ash <thejamesash1@gmail.com>
2025-03-19 19:39:15 -0400

    [react] create message component

---
+++ React/src/components/MessageComponent.tsx
@@ -0,0 +1,29 @@
+import classes from "../styles/application.module.css";
+import React from "react";
+import { Message } from "../../../Types/serverTypes";
+
+interface MessageComponentProps {
+  message: Message;
+}
+
+const MessageComponent: React.FC<MessageComponentProps> = ({ message }) => {
+  return (
+    <div className={classes.message}>
+      <img className={classes.squircle} src={message.author.avatar} alt="" />
+      <div className={classes.message_details}>
+        <div className={classes.author_details}>
+          <h3>
+            {message.author.firstName} {message.author.lastName}
+          </h3>
+          <p>{new Date(message.createdAt).toLocaleString()}</p>
+        </div>
+
+        <div className={classes.message_content}>
+          <p>{message.content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MessageComponent;
