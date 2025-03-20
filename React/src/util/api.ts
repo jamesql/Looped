ee98a5ccb04e378a04c36cd3fd3625eba35a3a55
James Ash <thejamesash1@gmail.com>
2025-03-20 18:46:00 -0400

    [react/api-client] add avatar to edit user

--- React/src/util/api.ts
+++ React/src/util/api.ts
@@ -61,6 +61,7 @@ class ApiClient {
             firstName: firstName,
             lastName: lastName,
             location: location,
+            avatar: avatar,
             status: status
         });
     }
