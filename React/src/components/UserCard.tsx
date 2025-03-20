d187fb1d28b87b1755cc5c99d94a23e22d608317
James Ash <thejamesash1@gmail.com>
2025-03-20 18:45:40 -0400

    [react] add default image

--- React/src/components/UserCard.tsx
+++ React/src/components/UserCard.tsx
@@ -10,7 +10,7 @@ const UserCard: React.FC<UserCardProps> = ({ user }) => {
     return (
         <li className={classes.member_card}>
         <div className={classes.member_image}>
-            <img className={classes.squircle} src={user.avatar} alt="" />
+            <img className={classes.squircle} src={user.avatar?user.avatar:"/logo_main.jpg"} alt="" />
         </div>
         <div className={classes.member_info}>
             <h3>{user.firstName} {user.lastName}</h3>
