d874c1c4b89a76ba8b84a23f8700a19cd8d055ae
James Ash <thejamesash1@gmail.com>
2025-03-21 00:55:44 -0400

    [api/data] add datapacks

--- API/data/data.ts
+++ API/data/data.ts
@@ -1,3 +1,21 @@
+import { User } from "../../Types/userTypes";
+
 export type RelationMap<T> = {
-    [K in keyof T]?: boolean | (T[K] extends Array<infer U> ? RelationMap<U>[] : RelationMap<T[K]>);
+    [K in keyof T]?: boolean | (T[K] extends Array<infer U> ? RelationMap<U> : RelationMap<T[K]>);
 };
+
+export class UserDatapacks {
+    public static readonly ALL_USER_DATA: RelationMap<User> = {
+        servers: {
+            channels: {
+                messages: {
+                    author: true
+                }
+            }
+        },
+        roles: true
+     
+
+    }
+    public static readonly USER_PUBLIC_DATA: RelationMap<User> = {}
+}
