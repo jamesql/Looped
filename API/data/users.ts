20920c7940584bd710260d39817076977dd74406
James Ash <thejamesash1@gmail.com>
2025-03-21 01:40:15 -0400

    [api/users] delete old data methods, fix some typing

--- API/data/users.ts
+++ API/data/users.ts
@@ -1,58 +1,13 @@
-import { Prisma, PrismaClient, User } from "@prisma/client";
-import { User as LoopedUser } from "../../Types/userTypes";
-import { Message, Server } from "../../Types/serverTypes";
-import { Channel } from "../../Types/serverTypes";
-import { Role } from "../../Types/serverTypes";
-
-import LoopedSession from "../../Types/sessionTypes";
+import { Prisma, PrismaClient, User as PrismaUser } from "@prisma/client";
+import { User } from "../../Types/userTypes";
 import { MapRMapToPMap, RelationMap } from "./data";
 
 const prisma = new PrismaClient();
 
 class UserService {
-  async createUser(
-    data: Omit<User, "id" | "createdAt" | "updatedAt" | "status">
-  ): Promise<User> {
-    return await prisma.user.create({
-      data,
-    });
-  }
-
-  async getUserById(id: string): Promise<User | null> {
-    return await prisma.user.findUnique({
-      where: { id },
-    });
-  }
-
-  async getUserByEmail(email: string): Promise<User | null> {
-    return await prisma.user.findUnique({
-      where: { email },
-    });
-  }
-
-  async getAllUsers(): Promise<User[]> {
-    return await prisma.user.findMany();
-  }
-
-  async updateUser(
-    id: string,
-    data: Partial<Omit<User, "id" | "createdAt" | "updatedAt">>
-  ): Promise<User> {
-    return await prisma.user.update({
-      where: { id },
-      data,
-    });
-  }
-
-  async deleteUser(id: string): Promise<User> {
-    return await prisma.user.delete({
-      where: { id },
-    });
-  }
-
   /** New user crud methods to map to global types */
 
-  async _createUser(user: LoopedUser): Promise<LoopedUser> {
+  async createUser(user: User): Promise<User> {
     return await prisma.user.create({
       data: {
         firstName: user.firstName,
@@ -67,10 +22,10 @@ class UserService {
     });
   }
 
-  async _getUserById(
+  async getUserById(
     id: string,
-    relation: RelationMap<LoopedUser>
-  ): Promise<LoopedUser> {
+    relation: RelationMap<User>
+  ): Promise<User> {
     const inc: Prisma.UserInclude = await MapRMapToPMap(relation);
     console.log(inc);
 
@@ -82,34 +37,27 @@ class UserService {
     });
   }
 
-  async _updateUserById(
+  async updateUserById(
     id: string,
-    data: Partial<LoopedUser>
-  ): Promise<LoopedUser> {
-    const updateData: Partial<User> = {};
-    for (const key in data) {
-      if (data[key] && key in updateData) {
-        updateData[key] = data[key];
-      }
-    }
-
+    data: Partial<User>
+  ): Promise<User> {
     return await prisma.user.update({
       where: { id },
       data: {
-        ...updateData,
+        ...data as Partial<PrismaUser>
       },
     });
   }
 
-  async _deleteUserById(id: string): Promise<void> {
+  async deleteUserById(id: string): Promise<void> {
     await prisma.user.delete({
       where: { id },
     });
     return;
   }
 
-  async _getUserByEmail(email: string, relation: RelationMap<LoopedUser>): Promise<LoopedUser> {
-    const inc = await this.MapRMapToPrismaUser(relation);
+  async getUserByEmail(email: string, relation: RelationMap<User>): Promise<User> {
+    const inc: Prisma.UserInclude = await MapRMapToPMap(relation);
 
     return await prisma.user.findUnique({
       where: { email },
@@ -117,32 +65,6 @@ class UserService {
         ...inc,
       },
     });
-  }
-
-  async MapRMapToPrismaUser(
-    relation: RelationMap<Omit<LoopedUser, "id" | "createdAt" | "updatedAt">>
-  ): Promise<Prisma.UserInclude> {
-    const include: Prisma.UserInclude = {};
-
-    // map relation to the include, include all rescursive relations in RelationMap
-    for (const key in relation) {
-      if (relation[key] === true) {
-        include[key] = true;
-      }
-      // if the value is an object, recursively map it
-      else if (typeof relation[key] === "object") {
-        include[key] = {
-          include: await this.MapRMapToPrismaUser(relation[key] as RelationMap<LoopedUser>)
-        };
-      }
-      // if the value is an array, map it as well
-      else if (Array.isArray(relation[key])) {
-        include[key] = {
-          include: await this.MapRMapToPrismaUser(relation[key][0] as RelationMap<LoopedUser>)
-        };
-      }
-    }
-    return include;
   }
 }
 
