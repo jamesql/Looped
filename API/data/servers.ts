2c9fb50c90703517193e655ad349212915a8ad66
James Ash <thejamesash1@gmail.com>
2025-03-21 02:28:27 -0400

    [api/data/server] remove old methods

--- API/data/servers.ts
+++ API/data/servers.ts
@@ -5,142 +5,8 @@ import { MapRMapToPMap, RelationMap } from "./data";
 const prisma = new PrismaClient();
 
 class ServerService {
-  async createServer(
-    data: Omit<Server, "id" | "createdAt" | "updatedAt">
-  ): Promise<Server> {
-    return await prisma.server.create({
-      data,
-    });
-  }
-
-  async getServerById(id: string): Promise<Server | null> {
-    return await prisma.server.findUnique({
-      where: { id },
-    });
-  }
-
-  async updateServer(id: string, data: Partial<Server>): Promise<Server> {
-    return await prisma.server.update({
-      where: { id },
-      data,
-    });
-  }
-
-  async editServer(id: string, data: Partial<Server>): Promise<Server> {
-    return await prisma.server.update({
-      where: { id },
-      data,
-    });
-  }
-
-  async deleteServer(id: string): Promise<Server> {
-    return await prisma.server.delete({
-      where: { id },
-    });
-  }
-
-  async getAllServers(): Promise<Server[]> {
-    return await prisma.server.findMany();
-  }
-
-  async addMember(serverId: string, userId: string): Promise<Server> {
-    return await prisma.server.update({
-      where: { id: serverId },
-      data: {
-        members: {
-          connect: { id: userId },
-        },
-      },
-    });
-  }
-
-  async getMember(serverId: string, userId: string): Promise<Server | null> {
-    return await prisma.server.findFirst({
-      where: {
-        id: serverId,
-        members: {
-          some: { id: userId },
-        },
-      },
-    });
-  }
-
-  async removeMember(serverId: string, userId: string): Promise<Server> {
-    return await prisma.server.update({
-      where: { id: serverId },
-      data: {
-        members: {
-          disconnect: { id: userId },
-        },
-      },
-    });
-  }
-
-  async banUser(serverId: string, userId: string): Promise<Server> {
-    return await prisma.server.update({
-      where: { id: serverId },
-      data: {
-        bannedUsers: {
-          connect: { id: userId },
-        },
-      },
-    });
-  }
-
-  async unbanUser(serverId: string, userId: string): Promise<Server> {
-    return await prisma.server.update({
-      where: { id: serverId },
-      data: {
-        bannedUsers: {
-          disconnect: { id: userId },
-        },
-      },
-    });
-  }
-
-  async getServerByInviteCode(code: string): Promise<Server | null> {
-    return await prisma.server.findFirst({
-      where: {
-        invites: {
-          has: code,
-        },
-      },
-    });
-  }
-
-  async addInviteCode(serverId: string, code: string): Promise<Server> {
-    return await prisma.server.update({
-      where: { id: serverId },
-      data: {
-        invites: {
-          push: code,
-        },
-      },
-    });
-  }
-
-  async getAllServerData(serverId: string): Promise<Server> {
-    return await prisma.server.findUnique({
-      where: { id: serverId },
-      include: {
-        members: true,
-        roles: true,
-        bannedUsers: true,
-        channels: {
-          include: {
-            messages: {
-              include: {
-                author: true,
-              },
-            },
-          },
-        },
-      },
-    });
-  }
-
   /** New functions  */
-  async _createServer(
+  async createServer(
     data: Omit<_Server, "id" | "createdAt" | "updatedAt">
   ): Promise<_Server> {
     return await prisma.server.create({
@@ -148,7 +14,7 @@ class ServerService {
     });
   }
 
-  async _getServerById(
+  async getServerById(
     id: string,
     relation: RelationMap<_Server>
   ): Promise<_Server> {
@@ -161,18 +27,78 @@ class ServerService {
     });
   }
 
-  async _editServerById(id: string, data: Partial<_Server>): Promise<_Server> {
+  async editServerById(id: string, data: Partial<_Server>): Promise<_Server> {
     return await prisma.server.update({
       where: { id },
       data: data as Partial<Server>,
     });
   }
 
-  async _deleteServerById(id: string): Promise<_Server> {
+  async deleteServerById(id: string): Promise<_Server> {
     return await prisma.server.delete({
       where: { id },
     });
   }
+
+  async getServerByInviteCode(code: string, relation: RelationMap<_Server>): Promise<_Server> {
+    const inc: Prisma.ServerInclude = await MapRMapToPMap(relation);
+    return await prisma.server.findFirst({
+      where: {
+        invites: {
+          has: code,
+        },
+      },
+      include: {
+        ...inc,
+      },
+    });
+  }
+
+  async addInviteCode(serverId: string, code: string): Promise<_Server> {
+    return await prisma.server.update({
+      where: { id: serverId },
+      data: {
+        invites: {
+          push: code,
+        },
+      },
+    });
+  }
+
+  
+  async addMember(serverId: string, userId: string): Promise<_Server> {
+    return await prisma.server.update({
+      where: { id: serverId },
+      data: {
+        members: {
+          connect: { id: userId },
+        },
+      },
+    });
+  }
+
+  async getMember(serverId: string, userId: string): Promise<_Server> {
+    return await prisma.server.findFirst({
+      where: {
+        id: serverId,
+        members: {
+          some: { id: userId },
+        },
+      },
+    });
+  }
+
+  async removeMember(serverId: string, userId: string): Promise<_Server> {
+    return await prisma.server.update({
+      where: { id: serverId },
+      data: {
+        members: {
+          disconnect: { id: userId },
+        },
+      },
+    });
+  }
+
 }
 
 export default new ServerService();
