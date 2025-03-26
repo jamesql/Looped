c3ad26036409565cc8b8ad03277abc69284aad86
James Ash <thejamesash1@gmail.com>
2025-03-26 14:26:46 -0400

    [types/job] add job types

--- Types/jobTypes.ts
+++ Types/jobTypes.ts
@@ -1,21 +1,15 @@
+import { Server } from "./serverTypes";
 import { User } from "./userTypes";
 
 export interface Job {
     id: string;
-    serverId: string;
     title: string;
-    payStart: number;
-    payEnd: number;
     description: string;
-    createdAt: string;
-    updatedAt: string;
-    applicants: Applicant[];
-    questions?: string[];
-}
-
-export interface Applicant {
-    userId: string;
-    user: User;
-    files: string[];
-    answers: string[];
+    createdAt: Date;
+    updatedAt: Date;
+    location: string;
+    salary: string;
+    status: string;
+    server?: Server;
+    applicants?: User[];
 }
