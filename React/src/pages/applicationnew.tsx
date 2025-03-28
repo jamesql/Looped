3f45ae8bd5e3cd6265360d2393778af7dac10a7e
James Ash <thejamesash1@gmail.com>
2025-03-28 11:55:28 -0400

    [react] rename application and start adding components

---
+++ React/src/pages/applicationnew.tsx
@@ -0,0 +1,199 @@
+import React, { useEffect, useRef, useState } from "react";
+import LoopedSession from "../../../Types/sessionTypes";
+import { Channel, Server, Role } from "../../../Types/serverTypes";
+import { User } from "../../../Types/userTypes";
+import Cookies from "js-cookie";
+import Loader from "@/components/Loader";
+import classes from "../styles/application.module.css";
+import ServerInfo from "@/components/ServerInfo";
+import FriendsList from "@/components/FriendsList";
+import { Permissions } from "../../../Types/permissionsTypes";
+import ServerIcon from "@/components/ServerIcon";
+
+const ApplicationNew: React.FC = () => {
+  // data states
+  const [loading, setLoading] = useState(true);
+  const [authed, setAuthed] = useState(false);
+  const [session, setSession] = useState<LoopedSession | null>(null);
+  const [selectedServer, setSelectedServer] = useState<Server | null>(null);
+  const [selectedChannel, setSelectedChannel] = useState<Channel | null>(null);
+  const [selectedFriend, setSelectedFriend] = useState<User | null>(null);
+
+  // modal states
+  const [serverSettings, setServerSettings] = useState(false);
+  const [createChannel, setCreateChannel] = useState(false);
+  const [creatingServer, setCreatingServer] = useState(false);
+  const [joiningServer, setJoiningServer] = useState(false);
+
+  // move to different file
+  // check if user has certain permissions
+  const checkPermissions = (serverId: string, userRoles: Role[], permission: number): boolean => {
+    // Check if the user has the required permissions
+    for (const role of userRoles) {
+      // Assuming role.permissions is an array of permissions
+      if (role.permissions.includes(permission.toString())) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    const accessToken = Cookies.get("access_token");
+    const refreshToken = Cookies.get("refresh_token");
+
+    if (!accessToken || !refreshToken) {
+      // redirect to login page
+      window.location.href = "/login";
+    } else {
+      setAuthed(true);
+      // here we also want to add some api client logic
+      // to validate token and then try and refresh before sending back to login
+    }
+  }, []);
+
+  useEffect(() => {
+    // change back
+    if (authed) {
+      setLoading(false);
+    }
+  }, [authed, session]);
+
+  return (
+    <div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <div className={classes.container}>
+            <div className={classes.server_info}>
+              {!selectedServer ? (
+                <div className={classes.server_card}>
+                  <div className={classes.server_card_info}>
+                    <h1>Direct Messages</h1>
+                  </div>
+                </div>
+              ) : (
+                <div className={classes.server_card}>
+                  <ServerInfo selectedServer={selectedServer} />
+
+                  {session?.id === selectedServer?.ownerId && (
+                    <button
+                      className={classes.settings_icon}
+                      onClick={() => setServerSettings(true)}
+                    >
+                      <img src="/settings.svg" alt="Settings" />
+                    </button>
+                  )}
+                </div>
+              )}
+
+              {!selectedServer ? (
+                <FriendsList friends={[]} />
+              ) : (
+                <div className={classes.channel_list}>
+                  {(selectedServer.ownerId === session?.id || checkPermissions(selectedServer.id, session?.roles?session.roles:[], Permissions.ADMIN)) && (
+                    <div
+                      className={[classes.channel, classes.channel_create].join(
+                        " "
+                      )}
+                      onClick={() => setCreateChannel(true)}
+                    >
+                      +
+                    </div>
+                  )}
+
+                  {selectedServer?.channels
+                    ? selectedServer.channels.map((channel) => (
+                        <div
+                          className={[
+                            classes.channel,
+                            selectedChannel?.id === channel.id
+                              ? classes.channel_active
+                              : "",
+                          ].join(" ")}
+                          onClick={() => setSelectedChannel(channel)}
+                        >
+                          <h2 className={classes.channel_name}>
+                            # {channel.name.toLowerCase().split(" ").join("-")}
+                          </h2>
+                        </div>
+                      ))
+                    : ""}
+                </div>
+              )}
+            </div>
+
+              <div className={classes.application}>
+
+              <div className={classes.server_nav}>
+                <ul className={classes.server_container}>
+                  <li className={classes.divider}></li>
+                  <li
+                    className={[classes.squircle, classes.server_icon].join(
+                      " "
+                    )}
+                    onClick={() => {
+                      setSelectedServer(null);
+                      setSelectedChannel(null);
+                      setSelectedFriend(null); 
+                    }}
+                  >
+                    <div className={classes.popper}>
+                      <h4 className={classes.popped}>Home</h4>
+                    </div>
+                  </li>
+                  <li className={classes.divider}></li>
+                  <li
+                    className={[classes.squircle, classes.server_icon].join(
+                      " "
+                    )}
+                    onClick={() => setJoiningServer(true)}
+                  >
+                    <div className={classes.popper}>
+                      <h4 className={classes.popped}>Join Server</h4>
+                    </div>
+                  </li>
+                  <li className={classes.divider}></li>
+                  <li
+                    className={[classes.squircle, classes.server_icon].join(
+                      " "
+                    )}
+                    onClick={() => setCreatingServer(true)}
+                  >
+                    <div className={classes.popper}>
+                      <h4 className={classes.popped}>Create Server</h4>
+                    </div>
+                  </li>
+                  <li className={classes.divider}></li>
+
+                  {session?.servers?.map((s) => (
+                    <ServerIcon server={s} 
+                    setSelectedServer={setSelectedServer} 
+                    setSelectedChannel={setSelectedChannel}
+                    selectedServer={selectedServer}
+                    selectedChannel={selectedChannel}
+                     />
+                  ))}
+                </ul>
+              </div>
+
+
+
+              </div>
+
+
+                  <div className={classes.members_profile}>
+
+                  </div>
+
+
+
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ApplicationNew;
