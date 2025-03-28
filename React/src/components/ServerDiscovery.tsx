c190328e6d8693436d643e3e5907bf5d43598f08
James Ash <thejamesash1@gmail.com>
2025-03-28 12:39:04 -0400

    [react] create server discovery

---
+++ React/src/components/ServerDiscovery.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { User } from '../../../Types/userTypes';
+
+interface ServerDiscoveryProps {
+
+}
+
+const ServerDiscovery: React.FC<ServerDiscoveryProps> = ({  }) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ServerDiscovery;
