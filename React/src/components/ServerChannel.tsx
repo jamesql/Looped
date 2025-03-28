47604f4816d8d18f02489cce05acb0c7bfafd7ad
James Ash <thejamesash1@gmail.com>
2025-03-28 12:38:57 -0400

    [react] create serverchannel

---
+++ React/src/components/ServerChannel.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { User } from '../../../Types/userTypes';
+
+interface ServerChannelProps {
+
+}
+
+const ServerChannel: React.FC<ServerChannelProps> = ({  }) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ServerChannel;
