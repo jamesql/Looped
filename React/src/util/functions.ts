47855c5211a9cfae8a0e022aa602feaff488c199
James Ash <thejamesash1@gmail.com>
2025-03-28 13:59:21 -0400

    [react] fix check permissions function

--- React/src/util/functions.ts
+++ React/src/util/functions.ts
@@ -1,14 +1,26 @@
 import { Role, Server } from "../../../Types/serverTypes";
+import { Permissions } from "../../../Types/permissionsTypes";
 
   // check if user has certain permissions
 export const checkPermissions = (server: Server, userId: string, userRoles: Role[], permission: number): boolean => {
-    if (server.ownerId === userId) return true;
-    // Check if the user has the required permissions
-    for (const role of userRoles) {
-      // Assuming role.permissions is an array of permissions
-      if (role.serverId === server.id && role.permissions === permission) {
-        return true;
-      }
+    // check if the user is the owner of the server
+    if (server.ownerId === userId) {
+        return true; // owner has all permissions
     }
+
+    const serverRoles = userRoles.filter((role) => role.serverId === server.id); // filter roles that belong to the server
+
+    if (serverRoles.length === 0) return permission === Permissions.MEMBER;
+
+
+    // check if user has any roles in the server
+    const role = serverRoles.find((role) => (role.permissions & permission) === permission);
+
+    // if a role with the required permission is found, return true
+    if (role) {
+        return true;
+    }
+
+    // otherwise return false
     return false;
   };
