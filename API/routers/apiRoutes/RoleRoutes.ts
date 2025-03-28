1ca168664cbf49ad96b65515db36555c580d11a3
James Ash <thejamesash1@gmail.com>
2025-03-28 16:34:48 -0400

    [api/roles] finish role routes

--- API/routers/apiRoutes/RoleRoutes.ts
+++ API/routers/apiRoutes/RoleRoutes.ts
@@ -90,13 +90,16 @@ router.post(
     // Call the RoleService to create the role in the database
     const role = await RoleService.createRole(newRole);
 
-    redisInstance.publish(`server:${server.id}:events`, JSON.stringify({
+    redisInstance.publish(
+      `server:${server.id}:events`,
+      JSON.stringify({
         op: OPCodes.ROLE_CREATE,
         d: {
-            server: server,
-            role: role 
-        }
-    }));
+          server: server,
+          role: role,
+        },
+      })
+    );
 
     res.status(200).json(role);
     return;
@@ -184,10 +187,10 @@ router.post(
 
     // edit the role
     const updatedRole: Role = {
-        ...role, // Spread the existing role properties
-        name: req.body.name, // Update the name
-        permissions: req.body.permissions, // Update the permissions
-    }
+      ...role, // Spread the existing role properties
+      name: req.body.name, // Update the name
+      permissions: req.body.permissions, // Update the permissions
+    };
 
     // Call the RoleService to update the role in the database
     const newRole = await RoleService.updateRole(
@@ -202,30 +205,36 @@ router.post(
     }
     // Successfully updated the role
 
-    redisInstance.publish(`server:${server.id}:events`, JSON.stringify({
+    redisInstance.publish(
+      `server:${server.id}:events`,
+      JSON.stringify({
         op: OPCodes.ROLE_MODIFY,
         d: {
-            server: server,
-            role: newRole
-        }
-    }));
+          server: server,
+          role: newRole,
+        },
+      })
+    );
 
     res.status(200).json(newRole);
     return;
   }
 );
 
-router.post("/delete", [
+router.post(
+  "/delete",
+  [
     header("Authorization").isString().isLength({ min: 1 }),
     body("roleId")
       .isString()
       .isLength({ min: 1 })
       .withMessage("Role ID is required."),
-], async (req: Request, res: Response) => {
+  ],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
-        return;
+      res.status(400).json({ errors: errors.array() });
+      return;
     }
 
     // validate access token
@@ -233,109 +242,311 @@ router.post("/delete", [
     const result = await validateToken(token);
 
     if (!result || !result.valid) {
-        res.status(401).json({ error: "Unauthorized" });
-        return;
+      res.status(401).json({ error: "Unauthorized" });
+      return;
     }
 
     // get user
     const user = await UserService.getUserById(
-        result.userId,
-        UserDatapacks.USER_PUBLIC_DATA
+      result.userId,
+      UserDatapacks.USER_PUBLIC_DATA
     );
 
     if (!user) {
-        res.status(404).json({ error: "User not found" });
-        return;
+      res.status(404).json({ error: "User not found" });
+      return;
     }
 
     // get role
     const role = await RoleService.getRoleById(req.body.roleId);
     if (!role) {
-        res.status(404).json({ error: "Role not found" });
-        return;
+      res.status(404).json({ error: "Role not found" });
+      return;
     }
 
     // get server
     const server = await ServerService.getServerById(
-        role.serverId,
-        ServerDatapacks.SERVER_PUBLIC_DATA
+      role.serverId,
+      ServerDatapacks.SERVER_PUBLIC_DATA
     );
     if (!server) {
-        res.status(404).json({ error: "Server not found" });
-        return;
+      res.status(404).json({ error: "Server not found" });
+      return;
     }
 
     // check if user is owner or admin
     if (server.ownerId !== user.id) {
-        // check if the user has admin permissions in the server
-        const roles = await RoleService.getUserRolesByServerId(
-            user.id,
-            server.id
-        );
-        if (
-            roles.length === 0 ||
-            !roles.some(
-                (role) => (role.permissions & Permissions.ADMIN) === Permissions.ADMIN
-            )
-        ) {
-            res.status(401).json({ error: "Unauthorized" });
-            return;
-        }
+      // check if the user has admin permissions in the server
+      const roles = await RoleService.getUserRolesByServerId(
+        user.id,
+        server.id
+      );
+      if (
+        roles.length === 0 ||
+        !roles.some(
+          (role) => (role.permissions & Permissions.ADMIN) === Permissions.ADMIN
+        )
+      ) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+      }
     }
 
     // make sure the role has no users who have it
     const usersWithRole = await UserService.getUsersByRoleId(req.body.roleId);
     // If there are users with this role, prevent deletion
     if (usersWithRole && usersWithRole.length > 0) {
-        // If there are users with this role, return an error
-        res.status(400).json({ error: "Cannot delete role, it is assigned to users." });
-        return;
+      // If there are users with this role, return an error
+      res
+        .status(400)
+        .json({ error: "Cannot delete role, it is assigned to users." });
+      return;
     }
 
     // Delete the role
     const deletedRole = await RoleService.deleteRole(req.body.roleId);
     if (!deletedRole) {
-        // If the deletion failed, return an error
-        res.status(500).json({ error: "Failed to delete role" });
-        return;
+      // If the deletion failed, return an error
+      res.status(500).json({ error: "Failed to delete role" });
+      return;
     }
 
-    redisInstance.publish(`server:${server.id}:events`, JSON.stringify({
+    redisInstance.publish(
+      `server:${server.id}:events`,
+      JSON.stringify({
         op: OPCodes.ROLE_DELETE,
         d: {
-            server: server,
-            role: deletedRole
-        }
-    }));
+          server: server,
+          role: deletedRole,
+        },
+      })
+    );
 
     // Successfully deleted the role
     res.status(200).json(deletedRole);
     return;
+  }
+);
 
-
-});
-
-
-router.post("/add-user", [
+router.post(
+  "/add-user",
+  [
     header("Authorization").isString().isLength({ min: 1 }),
-    body("roleId")
-      .isString()
-      .isLength({ min: 1 }),
-    body("userId")
-      .isString()
-      .isLength({ min: 1 })
-], async (req: Request, res: Response) => {
-});
+    body("roleId").isString().isLength({ min: 1 }),
+    body("userId").isString().isLength({ min: 1 }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
 
-router.post("/remove-user", [
+    // validate access token
+    const token = req.header("Authorization");
+    const result = await validateToken(token);
+    if (!result || !result.valid) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+
+    // get user
+    const user = await UserService.getUserById(
+      result.userId,
+      UserDatapacks.USER_PUBLIC_DATA
+    );
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    // get role
+    const role = await RoleService.getRoleById(req.body.roleId);
+    if (!role) {
+      res.status(404).json({ error: "Role not found" });
+      return;
+    }
+
+    // get server
+    const server = await ServerService.getServerById(
+      role.serverId,
+      ServerDatapacks.SERVER_PUBLIC_DATA
+    );
+
+    if (!server) {
+      res.status(404).json({ error: "Server not found" });
+      return;
+    }
+
+    // check if user is owner or admin
+    if (server.ownerId !== user.id) {
+      // check if the user has admin permissions in the server
+      const roles = await RoleService.getUserRolesByServerId(
+        user.id,
+        server.id
+      );
+      if (
+        roles.length === 0 ||
+        !roles.some(
+          (role) => (role.permissions & Permissions.ADMIN) === Permissions.ADMIN
+        )
+      ) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+      }
+    }
+
+    // Check if the user to be added exists
+    const userToAdd = await UserService.getUserById(
+      req.body.userId,
+      UserDatapacks.USER_PUBLIC_DATA
+    );
+    if (!userToAdd) {
+      // If the user to be added does not exist, return an error
+      res.status(404).json({ error: "User to add not found" });
+      return;
+    }
+
+    // Add the user to the role
+    const updatedUser = await RoleService.addRoleToUser(
+      req.body.userId, // The ID of the user to add
+      req.body.roleId // The ID of the role to assign
+    );
+    if (!updatedUser) {
+      // If adding the role to the user failed, return an error
+      res.status(500).json({ error: "Failed to add user to role" });
+      return;
+    }
+
+    // Successfully added the user to the role
+    // Publish the event to the server's event channel
+    redisInstance.publish(
+      `server:${server.id}:events`,
+      JSON.stringify({
+        op: OPCodes.ROLE_ADD,
+        d: {
+          server: server,
+          role: role,
+          user: updatedUser, // The updated user object
+        },
+      })
+    );
+
+    // Return the updated user
+    res.status(200).json(updatedUser);
+    return; 
+  }
+);
+
+router.post(
+  "/remove-user",
+  [
     header("Authorization").isString().isLength({ min: 1 }),
-    body("roleId")
-      .isString()
-      .isLength({ min: 1 }),
-    body("userId")
-      .isString()
-      .isLength({ min: 1 })
-], async (req: Request, res: Response) => {
-});
+    body("roleId").isString().isLength({ min: 1 }),
+    body("userId").isString().isLength({ min: 1 }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+
+    // validate access token
+    const token = req.header("Authorization");
+    const result = await validateToken(token);
+    if (!result || !result.valid) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+
+    // get user
+    const user = await UserService.getUserById(
+      result.userId,
+      UserDatapacks.USER_PUBLIC_DATA
+    );
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    // get role
+    const role = await RoleService.getRoleById(req.body.roleId);
+    if (!role) {
+      res.status(404).json({ error: "Role not found" });
+      return;
+    }
+
+    // get server
+    const server = await ServerService.getServerById(
+      role.serverId,
+      ServerDatapacks.SERVER_PUBLIC_DATA
+    );
+
+    if (!server) {
+      res.status(404).json({ error: "Server not found" });
+      return;
+    }
+
+    // check if user is owner or admin
+    if (server.ownerId !== user.id) {
+      // check if the user has admin permissions in the server
+      const roles = await RoleService.getUserRolesByServerId(
+        user.id,
+        server.id
+      );
+      if (
+        roles.length === 0 ||
+        !roles.some(
+          (role) => (role.permissions & Permissions.ADMIN) === Permissions.ADMIN
+        )
+      ) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+      }
+    }
+
+    // Check if the user to be added exists
+    const userToAdd = await UserService.getUserById(
+      req.body.userId,
+      UserDatapacks.USER_PUBLIC_DATA
+    );
+    if (!userToAdd) {
+      // If the user to be added does not exist, return an error
+      res.status(404).json({ error: "User to add not found" });
+      return;
+    }
+
+    // Remove the user from the role
+    const updatedUser = await RoleService.removeRoleFromUser(
+      req.body.userId, // The ID of the user to remove
+      req.body.roleId // The ID of the role to remove
+    );
+
+    if (!updatedUser) {
+      // If removing the role from the user failed, return an error
+      res.status(500).json({ error: "Failed to remove user from role" });
+      return;
+    }
+    // Successfully removed the user from the role
+    // Publish the event to the server's event channel
+
+    redisInstance.publish(
+      `server:${server.id}:events`,
+      JSON.stringify({
+        op: OPCodes.ROLE_REMOVE,
+        d: {
+          server: server,
+          role: role,
+          user: updatedUser, // The updated user object
+        },
+      })
+    );
+    // Return the updated user
+
+    res.status(200).json(updatedUser);
+    return;
+  }
+);
 
 module.exports = router;
