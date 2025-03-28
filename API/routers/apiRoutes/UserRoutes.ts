05af6f7fbf9cfb0a4819dcc3b52d353006c73ef4
James Ash <thejamesash1@gmail.com>
2025-03-28 13:52:03 -0400

    [api] remove restriction on avatar for now

--- API/routers/apiRoutes/UserRoutes.ts
+++ API/routers/apiRoutes/UserRoutes.ts
@@ -80,7 +80,7 @@ router.post("/edit", [
     body("lastName").isString().isLength({ min: 1 }),
     body("location").isString().isLength({ min: 1 }),
     body("status").isString().isLength({ min: 1 }),
-    body("avatar").isString().isLength({ min: 1 }),
+    body("avatar").isString().isLength({ min: 0 }),
 ], async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
