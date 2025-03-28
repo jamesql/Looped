4092cb1f821b16f6a142ca43a070ae669fbbc924
James Ash <thejamesash1@gmail.com>
2025-03-28 12:38:50 -0400

    [react] create direct channel

---
+++ React/src/components/DirectChannel.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { User } from '../../../Types/userTypes';
+
+interface DirectChannelProps {
+
+}
+
+const DirectChannel: React.FC<DirectChannelProps> = ({  }) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DirectChannel;
