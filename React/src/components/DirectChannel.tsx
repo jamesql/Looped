3aab290901e54dc33674847be20f46a23329dbd2
yubie-re <24496817+yubie-re@users.noreply.github.com>
2025-03-28 22:42:57 -0400

    [react] implement chat bar etc in dm

--- React/src/components/DirectChannel.tsx
+++ React/src/components/DirectChannel.tsx
@@ -1,16 +1,91 @@
-import React from 'react';
 import { DirectChannel as DChannel, User } from '../../../Types/userTypes';
+import React, { useState, useRef } from "react";
+import { Channel, Server } from "../../../Types/serverTypes";
+import Cookies from "js-cookie";
+import ApiClient from "@/util/api";
+import classes from "../styles/application.module.css";
+import MessageComponent from "./MessageComponent";
+import { MdAttachFile, MdSend } from "react-icons/md";
+import { uploadCdnFile } from "@/util/functions";
+
 
 interface DirectChannelProps {
     friend : User;
 }
 
 const DirectChannel: React.FC<DirectChannelProps> = ({ friend }) => {
+    const [currentMessage, setCurrentMessage] = useState("");
+    const fileInput = useRef<HTMLInputElement>(null);
+  
+    const sendMessage = (fileId?: string): void => {
+      if (currentMessage.trim() !== "" || fileId !== undefined) {
+        console.log("Sending message:", currentMessage);
+  
+        setCurrentMessage("");
+        // clear input box with class message_input
+        (
+          document.querySelector("." + classes.message_input) as HTMLInputElement
+        ).value = "";
+      }
+    };
+  
+    // File uploading
+    const handleFileButtonClick = () => {
+      fileInput?.current?.click();
+    };
+  
+    const handleFileChange = async (
+      event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      const files = event.target.files;
+      if (files && files.length > 0) {
+        for(let i = 0; i < files.length; i++) {
+          const file = files[i];
+          const cdnResp = await uploadCdnFile(file);
+          sendMessage(cdnResp.r2file.id);
+        }
+      }
+    };
+  
     return (
-        <div>
-            <h1>Direct Channel</h1>
-            <p>{friend.firstName}</p>
+      <>
+        <div className={classes.messages}>
         </div>
+  
+        <div className={classes.chat_input}>
+          <button
+            className={classes.chat_bar_button}
+            onClick={handleFileButtonClick}
+          >
+            <MdAttachFile className={classes.chat_bar_icon} />
+          </button>
+          <input
+            type="file"
+            ref={fileInput}
+            onChange={handleFileChange}
+            style={{ display: "none" }}
+            multiple
+          ></input>
+          <input
+            className={classes.message_input}
+            onChange={(e) => setCurrentMessage(e.target.value)}
+            type="text"
+            placeholder="Type a message..."
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
+          />
+          <button
+            className={classes.chat_bar_button}
+            onClick={() => sendMessage()}
+          >
+            <MdSend className={classes.chat_bar_icon} />
+          </button>
+        </div>
+      </>
+  
     );
 };
 
